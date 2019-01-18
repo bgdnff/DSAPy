adcd95910905752e8bdeb141a0e1c168d56c379c
Leo <bgdnff@gmail.com>
2019-01-18 07:46:40 +0500

    эратосфена сделал

--- Lesson4/errato_sphen.py
+++ Lesson4/errato_sphen.py
@@ -38,13 +38,20 @@ def non_sieve(count):
             primes.append(current)
         current += 1
     return primes[len(primes)-1]
-#print(sieve(1))
-cProfile.run('sieve(1000)')
+#print(sieve(100000))
+#cProfile.run('sieve(100000)')
 # 1    0.010    0.010    0.011    0.011 errato_sphen.py:14(sieve) - 1000
 # 1    0.023    0.023    0.026    0.026 errato_sphen.py:14(sieve) - 2000
 # 1    0.046    0.046    0.051    0.051 errato_sphen.py:14(sieve) - 4000
 # 1    0.067    0.067    0.073    0.073 errato_sphen.py:14(sieve) - 5000
 # 1    0.137    0.137    0.155    0.155 errato_sphen.py:14(sieve) - 10000
+# 1    2.314    2.314    2.613    2.613 errato_sphen.py:14(sieve) - 100000
+# 100 loops, best of 3: 9.16 msec per loop - 1000
+# 100 loops, best of 3: 20.8 msec per loop - 2000
+# 100 loops, best of 3: 49.5 msec per loop - 4000
+# 100 loops, best of 3: 63.9 msec per loop - 5000
+# 100 loops, best of 3: 147 msec per loop - 10000
+#   возрастает с неболшим ускорением... видимо, это как раз O(n*log(n))
 #print(non_sieve(1))
 #cProfile.run('non_sieve(4000)')
 # 1    0.100    0.100    0.102    0.102 errato_sphen.py:32(non_sieve) - 1000
@@ -52,3 +59,8 @@ cProfile.run('sieve(1000)')
 # 1    1.872    1.872    1.885    1.885 errato_sphen.py:32(non_sieve) - 4000
 # 1    3.269    3.269    3.282    3.282 errato_sphen.py:32(non_sieve) - 5000
 # 1   15.924   15.924   15.953   15.953 errato_sphen.py:32(non_sieve) - 10000
+# 100 loops, best of 3: 25.5 msec per loop - 500
+# 100 loops, best of 3: 95.5 msec per loop - 1000
+# 100 loops, best of 3: 379 msec per loop - 2000
+# 100 loops, best of 3: 1.69 sec per loop - 4000
+# возрастает довольно быстро, похоже на O(n**2)
